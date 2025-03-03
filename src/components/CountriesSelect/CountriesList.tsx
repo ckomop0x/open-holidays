97fe1e40a8201bc0514ca6d069e756f00fd8e8a3
Pavel Klochkov <paul.klochkov@gmail.com>
2025-03-03 15:26:04 +0100

    Refactor CountriesList component to use react-select for improved country selection

--- src/components/CountriesSelect/CountriesList.tsx
+++ src/components/CountriesSelect/CountriesList.tsx
@@ -1,44 +1,59 @@
-import { FC } from "react";
+import { FC, JSX } from "react";
 import { Country } from "@/types.ts";
 import ReactCountryFlag from "react-country-flag";
+import Select, { SingleValue } from "react-select";
 import { useCountry } from "@/hooks/useCountry.tsx";
+import { config } from "@/config/config.ts";
 
 interface CountriesListProps {
   countries: Country[];
 }
 
-const CountriesList: FC<CountriesListProps> = ({ countries }) => {
-  const { selectedCountry, setSelectedCountry } = useCountry();
+type OptionType = {
+  value: string;
+  label: JSX.Element;
+};
 
-  const buttonClass =
-    "border border-gray-300 rounded-md p-2 m-1 cursor-pointer hover:bg-gray-200";
-  const selectedButtonClass =
-    "border border-gray-300 rounded-md p-2 m-1 bg-gray-200 cursor-pointer";
-  const isCountrySelected = (isoCode: string) => selectedCountry === isoCode;
+const CountriesList: FC<CountriesListProps> = ({ countries }) => {
+  const { setSelectedCountry } = useCountry();
+
+  console.log(countries);
+
+  const options: OptionType[] = countries.map((country) => ({
+    value: country.isoCode,
+    label: (
+      <div className="flex items-center">
+        <ReactCountryFlag
+          countryCode={country.isoCode}
+          className="w-6 h-4 mr-2"
+          svg
+        />
+        {country.name[0].text}
+      </div>
+    ),
+  }));
+
+  const onChange = (newValue: SingleValue<OptionType>) => {
+    if (!newValue) return;
+
+    setSelectedCountry(newValue.value);
+  };
 
   return (
     <>
       {countries.length ? (
         <div className="flex flex-wrap flex-row container justify-center mx-auto">
-          {countries.map((country: Country) => (
-            <button
-              value={country.isoCode}
-              key={country.isoCode}
-              onClick={() => setSelectedCountry(country.isoCode)}
-              className={
-                isCountrySelected(country.isoCode)
-                  ? selectedButtonClass
-                  : buttonClass
-              }
-            >
-              <ReactCountryFlag
-                countryCode={country.isoCode}
-                title={country.name[0].text}
-                className="text-2xl leading-[2em]"
-                svg
-              />
-            </button>
-          ))}
+          <Select
+            isLoading={!countries?.length}
+            defaultValue={
+              options.filter(
+                (option) => option.value === config.defaultCountry,
+              )[0]
+            }
+            onChange={onChange}
+            options={options}
+            className="w-60"
+          />
         </div>
       ) : (
         <div className="block text-sm font-medium text-gray-700 border-1 border-gray-300 rounded-md p-2">
