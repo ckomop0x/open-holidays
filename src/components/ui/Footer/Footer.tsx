ef73576db4d768f2059d73bb933089eea2035084
Pavel Klochkov <paul.klochkov@gmail.com>
2025-05-13 15:01:36 +0200

    Final footer polishing

--- src/components/ui/Footer/Footer.tsx
+++ src/components/ui/Footer/Footer.tsx
@@ -1,33 +1,29 @@
 import { FC } from "react";
 import { getAppVersion } from "@/lib/getAppVersion";
 
-const Footer: FC = () => {
-  return (
-    <footer className="bg-[#154273] text-white text-center h-18 flex justify-center items-center">
-      <div className="flex flex-col">
-        <p>
-          &copy; 2025 Pavel Klochkov,&nbsp;
-          <a
-            href="https://github.com/ckomop0x/open-holidays-2025"
-            target="_blank"
-            className="underline"
-          >
-            GitHub
-          </a>
-          ,&nbsp;
-          <a
-            href="https://www.openholidaysapi.org/en/"
-            rel="noopener noreferrer"
-            target="_blank"
-            className="underline"
-          >
-            OpenHolidays API
-          </a>
-        </p>
-        <p className="text-xs">v. {getAppVersion()}</p>
-      </div>
-    </footer>
-  );
-};
+const Footer: FC = () => (
+  <footer className="bg-[#154273] text-white h-18 flex flex-col w-full items-center justify-center">
+    <p className="w-full max-w-md px-6 text-center">
+      &copy; 2025 Pavel Klochkov,&nbsp;
+      <a
+        href="https://github.com/ckomop0x/open-holidays-2025"
+        target="_blank"
+        className="underline"
+      >
+        GitHub
+      </a>
+      ,&nbsp;
+      <a
+        href="https://www.openholidaysapi.org/en/"
+        rel="noopener noreferrer"
+        target="_blank"
+        className="underline"
+      >
+        OpenHolidays API
+      </a>
+    </p>
+    <p className="text-xs text-center">v{getAppVersion()}</p>
+  </footer>
+);
 
 export default Footer;
