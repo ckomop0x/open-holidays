6674f4ecc3571ae0beb63f06e075112255b0e367
Pavel Klochkov <ckomop0x@users.noreply.github.com>
2025-05-13 13:59:38 +0200

    fix: update container height to use dynamic viewport height (#108)

--- src/components/ui/Container/Container.tsx
+++ src/components/ui/Container/Container.tsx
@@ -1,7 +1,7 @@
 import { FC, PropsWithChildren } from "react";
 
 const Container: FC<PropsWithChildren> = ({ children }) => (
-  <div className="flex flex-col relative w-full h-full">{children}</div>
+  <div className="flex flex-col relative w-full h-dvh">{children}</div>
 );
 
 export default Container;
