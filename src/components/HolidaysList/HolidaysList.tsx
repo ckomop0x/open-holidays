1dd41665ef17b4ac51a15d2b6402dc987aa600f7
Pavel Klochkov <ckomop0x@users.noreply.github.com>
2025-05-13 15:02:32 +0200

    Some visual changes for better horisontal aligment (#109)
    * Some visual changes for better horisontal aligment
    
    * A bit more horisonatal padding
    
    * Visual polishing
    
    * Update main title
    
    * Visual polishing
    
    * Change title font a bit
    
    * Use full available width
    
    * Final footer polishing

--- src/components/HolidaysList/HolidaysList.tsx
+++ src/components/HolidaysList/HolidaysList.tsx
@@ -8,14 +8,14 @@ const HolidaysList: FC = () => {
 
   return (
     <div className="flex flex-col mt-2">
-      <div className="max-w-lg mx-auto p-4">
+      <div className="mx-auto py-4 w-full">
         {Object.entries(getGroupedHolidays(holidays)).map(
           ([month, monthHolidays]) => (
             <div key={month} className="mb-4">
               <h2 className="text-lg font-bold border-b border-b-gray-300 pb-2 mb-2">
                 {month}
               </h2>
-              <div className="grid grid-cols-4">
+              <div className="grid grid-cols-4 gap-x-4">
                 {monthHolidays.map((holiday) => {
                   const formattedDate = getFormattedHolidayDate(
                     holiday.startDate,
